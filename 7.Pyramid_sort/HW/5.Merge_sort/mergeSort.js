423adff01361ffae1ffa7a33de02faf6f480c0ca
Natalya Myunster <natalyamyunster@yahoo.com>
2023-03-06 20:14:43 +0100

    added mergeSort

--- 7.Pyramid_sort/HW/5.Merge_sort/mergeSort.js
+++ 7.Pyramid_sort/HW/5.Merge_sort/mergeSort.js
@@ -1,14 +1,44 @@
 import {array10, array100, array1000, array10000,array100000} from '../utils.js';
 
-//O(N^2)/O(1)
+//O(n log(n))/O(N)
 function mergeSort(array) {
   const N = array.length;
   let cmp = 0;
   let asg = 0;
 
-  console.log(`cmp ${N}`, cmp);
-  console.log(`asg ${N}`, asg);
-  return array;
+  if(N <= 1) return array;
+  const mid = Math.floor(N/2);
+  console.log('mid', mid);
+  const left = mergeSort(array.slice(0,mid));
+  console.log('left', left);
+  const right = mergeSort(array.slice(mid));
+  console.log('right', right);
+
+  return merge(left, right);
+}
+
+function merge(arr1, arr2){
+  let results = [];
+  let i = 0;
+  let j = 0;
+  while(i < arr1.length && j < arr2.length){
+    if(arr2[j] > arr1[i]){
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j])
+      j++;
+    }
+  }
+  while(i < arr1.length) {
+    results.push(arr1[i])
+    i++;
+  }
+  while(j < arr2.length) {
+    results.push(arr2[j])
+    j++;
+  }
+  return results;
 }
 
 console.log(mergeSort([3, 9, 5, 12, 0, 11, 1, 4, 2]));
